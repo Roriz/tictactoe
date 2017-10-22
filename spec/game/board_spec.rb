66ccd8803bacba15b0fad2fd02a93fcffeef1b20
Radames Roriz <radamesroriz@gmail.com>
2017-10-22 16:37:59 -0200

    add specs

---
+++ spec/game/board_spec.rb
@@ -0,0 +1,166 @@
+require 'game/board'
+require 'game/player'
+
+RSpec.describe Game::Board, '#Game::Board' do
+  describe '#initialize' do
+    it 'should set empty board' do
+      board = Game::Board.new
+
+      state = board.instance_variable_get(:@board)
+
+      expect(state).to eq([nil, nil, nil, nil, nil, nil, nil, nil, nil])
+    end
+  end
+
+  describe '#update' do
+    it 'should update right position with player instance' do
+      board = Game::Board.new
+      player = Game::Player.new(type: :HUMAN, position: :player_1)
+
+      state = board.instance_variable_get(:@board)
+
+      board.update(action: 0, player: player)
+
+      expect(state).to eq([player, nil, nil, nil, nil, nil, nil, nil, nil])
+    end
+  end
+
+  describe '#current_state' do
+    it 'should exposing the current state of the board' do
+      board = Game::Board.new
+      player = Game::Player.new(type: :HUMAN, position: :player_1)
+
+      expect(board.current_state).to eq([nil, nil, nil, nil, nil, nil, nil, nil, nil])
+
+      board.update(action: 0, player: player)
+
+      expect(board.current_state).to eq([player, nil, nil, nil, nil, nil, nil, nil, nil])
+    end
+  end
+
+  describe '#empty_spaces' do
+    it 'should response array of index for all empty spaces' do
+      board = Game::Board.new
+      player = Game::Player.new(type: :HUMAN, position: :player_1)
+
+      expect(board.empty_spaces).to eq((0..8).to_a)
+
+      board.update(action: 0, player: player)
+
+      expect(board.empty_spaces).to eq((1..8).to_a)
+    end
+  end
+
+  describe '#empty_space?' do
+    it 'should response if has any empty space on current board' do
+      board = Game::Board.new
+      player = Game::Player.new(type: :HUMAN, position: :player_1)
+
+      expect(board.empty_space?).to be true
+
+      board.update(action: 0, player: player)
+      board.update(action: 1, player: player)
+      board.update(action: 2, player: player)
+      board.update(action: 3, player: player)
+      board.update(action: 4, player: player)
+      board.update(action: 5, player: player)
+      board.update(action: 6, player: player)
+      board.update(action: 7, player: player)
+      board.update(action: 8, player: player)
+
+      expect(board.empty_space?).to be false
+    end
+  end
+
+  describe '#winner' do
+    it 'should request winner from Board class with current state' do
+      board = Game::Board.new
+
+      expect(Game::Board).to receive(:winner)
+        .with(board: board.current_state).and_return(0)
+
+      board.winner
+    end
+  end
+
+  describe '.winner' do
+    it 'should response player winner when player are on [0,1,2] positions' do
+      player = Game::Player.new(type: :HUMAN, position: :player_1)
+
+      board = [player, player, player, nil, nil, nil, nil, nil, nil]
+
+      expect(Game::Board.winner(board: board)).to be player
+    end
+
+    it 'should response player winner when player are on [3,4,5] positions' do
+      player = Game::Player.new(type: :HUMAN, position: :player_1)
+
+      board = [nil, nil, nil, player, player, player, nil, nil, nil]
+
+      expect(Game::Board.winner(board: board)).to be player
+    end
+
+    it 'should response player winner when player are on [6, 7, 8] positions' do
+      player = Game::Player.new(type: :HUMAN, position: :player_1)
+
+      board = [nil, nil, nil, nil, nil, nil, player, player, player]
+
+      expect(Game::Board.winner(board: board)).to be player
+    end
+
+    it 'should response player winner when player are on [0, 3, 6] positions' do
+      player = Game::Player.new(type: :HUMAN, position: :player_1)
+
+      board = [player, nil, nil, player, nil, nil, player, nil, nil]
+
+      expect(Game::Board.winner(board: board)).to be player
+    end
+
+    it 'should response player winner when player are on [1, 4, 7] positions' do
+      player = Game::Player.new(type: :HUMAN, position: :player_1)
+
+      board = [nil, player, nil, nil, player, nil, nil, player, nil]
+
+      expect(Game::Board.winner(board: board)).to be player
+    end
+
+    it 'should response player winner when player are on [2, 5, 8] positions' do
+      player = Game::Player.new(type: :HUMAN, position: :player_1)
+
+      board = [nil, nil, player, nil, nil, player, nil, nil, player]
+
+      expect(Game::Board.winner(board: board)).to be player
+    end
+
+    it 'should response player winner when player are on [0, 4, 8] positions' do
+      player = Game::Player.new(type: :HUMAN, position: :player_1)
+
+      board = [player, nil, nil, nil, player, nil, nil, nil, player]
+
+      expect(Game::Board.winner(board: board)).to be player
+    end
+
+    it 'should response player winner when player are on [2, 4, 6] positions' do
+      player = Game::Player.new(type: :HUMAN, position: :player_1)
+
+      board = [nil, nil, player, nil, player, nil, player, nil, nil]
+
+      expect(Game::Board.winner(board: board)).to be player
+    end
+
+    it 'should response nil when are two differents players on [0, 1, 2] positions' do
+      player1 = Game::Player.new(type: :HUMAN, position: :player_1)
+      player2 = Game::Player.new(type: :HUMAN, position: :player_2)
+
+      board = [player1, player1, player2, nil, nil, nil, nil, nil, nil]
+
+      expect(Game::Board.winner(board: board)).to be nil
+    end
+
+    it 'should response nil when board are empty' do
+      board = [nil, nil, nil, nil, nil, nil, nil, nil, nil]
+
+      expect(Game::Board.winner(board: board)).to be nil
+    end
+  end
+end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,22 +1,139 @@
 require 'game.rb'
 
 RSpec.describe Game::Game, '#Game' do
-  it 'should initialize with interface class' do
+  it 'should initialize with interface class and board class' do
     game = Game::Game.new
 
     interface = game.instance_variable_get(:@interface)
+    board = game.instance_variable_get(:@board)
 
     expect(interface).to be_an_instance_of(Game::Interface)
+    expect(board).to be_an_instance_of(Game::Board)
+  end
+
+  it 'should initialize with default players' do
+    game = Game::Game.new
+
+    players = game.instance_variable_get(:@players)
+    player1 = players.first
+    player2 = players.last
+
+    expect(players.length).to eq(2)
+    expect(player1).to be_an_instance_of(Game::Player)
+    expect(player1.human?).to eq(true)
+    expect(player1.instance_variable_get(:@position)).to eq(:player_1)
+    expect(player2).to be_an_instance_of(Game::Player)
+    expect(player2.human?).to eq(false)
+    expect(player2.instance_variable_get(:@position)).to eq(:player_2)
   end
 
   describe '#start_game' do
-    it 'should render empty board and request first input' do
+    it 'should render empty board' do
       game = Game::Game.new
 
       interface = game.instance_variable_get(:@interface)
+      board = game.instance_variable_get(:@board)
 
-      expect(interface).to receive(:render)
-      expect(interface).to receive(:request_input)
+      expect(interface).to receive(:render).with(board: board)
+      expect(board).to receive(:empty_space?).and_return(false)
+
+      game.start_game
+    end
+
+    it 'should request action for player1 at first' do
+      game = Game::Game.new
+
+      players = game.instance_variable_get(:@players)
+      board = game.instance_variable_get(:@board)
+
+      expect(board).to receive(:empty_space?).and_return(true, false)
+      expect(game).to receive(:player_action).with(player: players.first, opponent: players.last)
+
+      game.start_game
+    end
+
+    it 'should request action for player2 at second' do
+      game = Game::Game.new
+
+      players = game.instance_variable_get(:@players)
+      board = game.instance_variable_get(:@board)
+      game.instance_variable_set(:@turn, 1)
+
+      expect(board).to receive(:empty_space?).and_return(true, false)
+      expect(game).to receive(:player_action).with(player: players.last, opponent: players.first)
+
+      game.start_game
+    end
+
+    it 'should request action, update borad and render board when player action' do
+      game = Game::Game.new
+
+      players = game.instance_variable_get(:@players)
+      board = game.instance_variable_get(:@board)
+      interface = game.instance_variable_get(:@interface)
+
+      move = 0
+
+      expect(board).to receive(:empty_space?).and_return(true, false)
+      expect(game).to receive(:player_turn).with(player: players.first, opponent: players.last).and_return(move)
+      expect(board).to receive(:update).with(action: move, player: players.first)
+      expect(interface).to receive(:render).with(board: board).twice
+
+      game.start_game
+    end
+
+    it 'should request human interaction when player is human' do
+      game = Game::Game.new
+
+      board = game.instance_variable_get(:@board)
+      interface = game.instance_variable_get(:@interface)
+
+      expect(board).to receive(:empty_space?).and_return(true, false)
+      expect(interface).to receive(:request_input).and_return(0)
+
+      game.start_game
+    end
+
+    it 'should request best move when player is AI' do
+      game = Game::Game.new
+
+      player1 = Game::Player.new(
+        type: :AI,
+        position: :player_1
+      )
+      player2 = Game::Player.new(
+        type: :AI,
+        position: :player_2
+      )
+
+      board = game.instance_variable_get(:@board)
+      game.instance_variable_set(:@players, [player1, player2])
+
+      expect(board).to receive(:empty_space?).and_return(true, false)
+      expect(player1).to receive(:best_move).with(board: board, opponent: player2).and_return(0)
+
+      game.start_game
+    end
+
+    it 'should trigger end game when has winner' do
+      game = Game::Game.new
+
+      board = game.instance_variable_get(:@board)
+      players = game.instance_variable_get(:@players)
+
+      expect(game).to receive(:end_game)
+      expect(board).to receive(:winner).and_return(players.first)
+
+      game.start_game
+    end
+
+    it 'should trigger end game when not has empty space on board' do
+      game = Game::Game.new
+
+      board = game.instance_variable_get(:@board)
+
+      expect(game).to receive(:end_game)
+      expect(board).to receive(:empty_space?).and_return(false)
 
       game.start_game
     end
