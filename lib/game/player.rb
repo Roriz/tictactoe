18f0ccf5dcaf412383c3bdd1ca326c037cb8e28d
Radamés Roriz <radamesroriz@gmail.com>
2017-10-23 17:49:58 +0000

    Merged in feature/apply-ai-dynamic (pull request #7)
    Feature/multiples AI

--- lib/game/player.rb
+++ lib/game/player.rb
@@ -13,10 +13,27 @@ module Game
     }.freeze
 
     BOARD_BEST_ACTIONS = [
-      3, 2, 3,
-      2, 4, 2,
-      3, 2, 3
+      { base: 4, position: [4] },
+      { base: 3, position: [0, 2, 6, 8] },
+      { base: 2, position: [1, 3, 5, 7] }
     ].freeze
+
+    AI_SETTINGS = {
+      SELF_WINNER: :SELF_WINNER,
+      SELF_OPPONENT: :SELF_OPPONENT,
+      BEST_ACTION: :BEST_ACTION,
+      RANDOM_ACTION: :RANDOM_ACTION,
+      TROLL_ACTION: :TROLL_ACTION,
+      WORST_ACTION: :WORST_ACTION
+    }.freeze
+
+    AI_ACTIONS = {
+      easy: %i[WORST_ACTION],
+      medium: %i[RANDOM_ACTION],
+      hard: %i[RANDOM_ACTION SELF_WINNER SELF_OPPONENT],
+      impossible: %i[BEST_ACTION SELF_WINNER SELF_OPPONENT],
+      troll: %i[BEST_ACTION TROLL_ACTION]
+    }.freeze
 
     def initialize(type:, position:, type_of_ai: nil)
       @type = type
@@ -35,15 +52,14 @@ module Game
     def best_move(board:, opponent:)
       board_state = board.current_state.dup
 
-      best_move = board_state.find_index.with_index do |space, index|
-        if space.nil?
-          simulate_action(board: board_state.dup, opponent: opponent, action: index)
-        else
-          false
-        end
-      end
+      reactive_action = reactive_action(board: board_state, opponent: opponent)
+      best_action = nil
+      best_action = board.empty_spaces.sample if action_for(:RANDOM_ACTION)
+      best_action = best_action(board.empty_spaces) if action_for(:BEST_ACTION)
+      best_action = worst_action(board.empty_spaces) if action_for(:WORST_ACTION)
+      best_action = troll_action(board_state) if action_for(:TROLL_ACTION)
 
-      best_move || board.empty_spaces.sample
+      reactive_action || best_action
     end
 
     def apply_settings(settings:)
@@ -53,9 +69,47 @@ module Game
 
     private
 
+    def troll_action(board)
+      board.find_index { |pos| !pos.nil? && pos != self }
+    end
+
+    def worst_action(empty_spaces)
+      action = BOARD_BEST_ACTIONS.reverse.find { |a| !(empty_spaces & a[:position]).empty? }
+
+      action ? (empty_spaces & action[:position]).first : nil
+    end
+
+    def best_action(empty_spaces)
+      action = BOARD_BEST_ACTIONS.find { |a| !(empty_spaces & a[:position]).empty? }
+
+      action ? (empty_spaces & action[:position]).first : nil
+    end
+
+    def reactive_action(board:, opponent:)
+      board.find_index.with_index do |space, index|
+        if space.nil?
+          simulate_action(board: board.dup, opponent: opponent, action: index)
+        else
+          false
+        end
+      end
+    end
+
+    def action_for(action)
+      AI_ACTIONS[@type_of_ai].include?(action)
+    end
+
     def simulate_action(board:, opponent:, action:)
-      self_winner = simulate_winner(board: board, player: self, action: action)
-      opponent_winner = simulate_winner(board: board, player: opponent, action: action)
+      self_winner = nil
+      opponent_winner = nil
+
+      if action_for(:SELF_WINNER)
+        self_winner = simulate_winner(board: board, player: self, action: action)
+      end
+
+      if action_for(:SELF_OPPONENT)
+        opponent_winner = simulate_winner(board: board, player: opponent, action: action)
+      end
 
       self_winner || opponent_winner
     end
